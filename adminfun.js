cd05e42d0b381e682d4401bc947e8d7b87301a66
DannyVinueza <danny.vinueza@epn.edu.ec>
2023-03-14 01:37:44 -0500

    Boton editar en administrador

--- adminfun.js
+++ adminfun.js
@@ -1,11 +1,20 @@
 const btnAbrirIng = document.querySelector(".ingre");
+const btnAbrirEdit = document.querySelector(".edita");
+
 const btnAbrirCon = document.querySelector("#btm-registrar-modal");
+const btnEditarCon = document.querySelector("#btm-editar-modal")
 
 const btnAbrirNue = document.querySelector("#btm-si");
 const btnCerrarModal = document.querySelector("#btm-no");
+const btnAbrirNueEdit = document.querySelector("#btm-si-edit");
+const btnCerrarEditCon = document.querySelector("#btm-no-edit");
+
 const modal = document.querySelector(".ingresar-modal");
 
 const modalconfir = document.querySelector(".confir-ingr");
+const modaledit = document.querySelector(".editar-modal");
+const modalconedit = document.querySelector(".confir-edit");
+
 
 btnAbrirIng.addEventListener("click",()=>{
     modal.showModal();
@@ -22,4 +31,23 @@ btnAbrirCon.addEventListener("click", ()=>{
 
 btnAbrirNue.addEventListener("click", ()=>{
     modalconfir.close();
+})
+
+btnAbrirEdit.addEventListener("click", ()=>{
+    modaledit.showModal();
+})
+
+btnEditarCon.addEventListener("click", ()=>{
+    modaledit.close();
+    modalconedit.showModal();
+})
+
+btnCerrarEditCon.addEventListener("click", ()=>{
+    modalconedit.close();
+    modaledit.showModal();
+})
+
+btnAbrirNueEdit.addEventListener("click", ()=>{
+    modaledit.close();
+    modalconedit.close();
 })
